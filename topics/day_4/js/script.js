eec1be22e5eeabcff3ae1a5f727656f51bc80875
Irish <irishmjn@gmail.com>
2023-12-06 10:49:15 +0545

    git@github.com:Erishprajapati/bca_sl_2023_topics.git

---
+++ topics/day_4/js/script.js
@@ -0,0 +1,31 @@
+//defininig array
+let places = ["Kathmandu","Lalitpur", "Pokhara", "Butwal"]
+const gender = ["Male","Female", "others"]
+
+//accessing array elements with indexing 
+console.log(places[0])
+console.log(places[1])
+console.log(places[3])
+
+//since array is an object so we can define it using array()
+let lakes = new Array("Phewa", "Begnas", "Rara", "Tilicho")
+
+//array properties and methods
+//properties
+
+//length- return the lenght of an array
+//example
+console.log(places.length)
+console.log(gender.length)
+console.log(lakes.length)
+
+//methods
+//1. push - return the lenght of an array example
+//example
+let days = ["Sunday","Monday"]
+console.log(days)
+days.push("Tuesday")
+console.log(days)
+days.push("Wednesday")
+console.log(days)
+
